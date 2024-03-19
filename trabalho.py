9dc4fbde6d322c45e94748e7f0e0e4c26d3285f2
Leticia2908 <159178285+Leticia2908@users.noreply.github.com>
2024-03-19 15:05:39 -0300

    Add files via upload

---
+++ trabalho.py
@@ -0,0 +1,25 @@
+numero = 10
+nome = "João"
+booleano = True
+
+
+a = True
+b = False
+resultado1 = a and b
+resultado2 = a or b
+resultado3 = not a
+
+a = 10
+b = 5
+
+# a ordem de procedencia dos operadores logicos: not, and, or
+# para modificar a ordem das operadocoes logicas é necessario usar parenteses
+not ( a > 1 and b < 5) 
+Out[19]: True
+( a > 1 b < 4 )
+  Cell In[21], line 1
+    ( a > 1 b < 4 )
+      
+
+
+
